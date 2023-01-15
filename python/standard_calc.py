b2c6f7d4a9f4b1ebaba24bbe26fe3520514f07a6
FireBoyAJ24 <63443017+FireBoyAJ24@users.noreply.github.com>
2023-01-15 11:08:00 -0800

    Adding my answer and formated to Flask8

--- python/standard_calc.py
+++ python/standard_calc.py
@@ -1,3 +1,4 @@
+
 def bound_to_180(angle):
     """Bounds the provided angle between [-180, 180) degrees.
 
@@ -11,7 +12,30 @@ def bound_to_180(angle):
     Returns:
         float: The bounded angle in degrees.
     """
-    return 0
+
+    # in360 bounds the angle within 0-360 degrees
+    # in180 bounds the angle within 0-180 degrees
+
+    if (angle < 0):  # Checks whether the angle is negative or positive
+        in360 = angle % -360.0  # Bounds the angle within 360 degrees
+        if (in360 < -180):
+            in180 = abs(angle) % 180
+        elif (in360 == -180):
+            #  in180 = -180 % -180 == 0
+            in180 = -180
+        else:
+            in180 = angle % -180
+    else:
+        in360 = angle % 360.0
+        if (in360 > 180):
+            in180 = -angle % -180
+        elif (in360 == 180):
+            #  in180 = 180 % 180 = 0 not -180
+            in180 = -180
+        else:
+            in180 = angle % 180
+
+    return in180
 
 
 def is_angle_between(first_angle, middle_angle, second_angle):
@@ -29,4 +53,24 @@ def is_angle_between(first_angle, middle_angle, second_angle):
     Returns:
         bool: True when `middle_angle` is not in the reflex angle of `first_angle` and `second_angle`, false otherwise.
     """
-    return True
+
+    first_angle, second_angle = bound_to_180(first_angle), bound_to_180(second_angle)
+
+    # Assuming all angles are positive
+    diff_angle = abs(first_angle - second_angle)
+    if (second_angle >= first_angle):
+        max_angle, min_angle = second_angle, first_angle
+    else:
+        max_angle, min_angle = first_angle, second_angle
+
+    if (diff_angle > 180):  # Reflex angle region
+        #  Checks whether the angle is outside the reflex angle region
+        if (middle_angle > max_angle or middle_angle < min_angle):
+            return True
+        else:
+            return False
+    else:
+        if (min_angle <= middle_angle <= max_angle):  # Checks whether within anti-reflex region.
+            return True
+        else:
+            return False
--- python/test_standard_calc.py
+++ python/test_standard_calc.py
@@ -6,10 +6,42 @@ from standard_calc import bound_to_180, is_angle_between
 
 def test_bound_basic1():
     assert bound_to_180(0) == 0
+    assert bound_to_180(-180) == -180
+    assert bound_to_180(180) == -180
+    assert bound_to_180(540) == -180
+    assert bound_to_180(720) == 0
+    assert bound_to_180(60) == 60
+    assert bound_to_180(-45) == -45
+    assert bound_to_180(120) == 120
+    assert bound_to_180(-125) == -125
+    assert bound_to_180(179) == 179
+    assert bound_to_180(-179) == -179
+    assert bound_to_180(-3779) == -179
+    assert bound_to_180(3779) == 179
+    assert bound_to_180(360) == 0
+    assert bound_to_180(-233) == 53
 
 
 """ Tests for is_angle_between() """
 
 
 def test_between_basic1():
-    assert is_angle_between(0, 1, 2)
+    """Checks different situations such as boundary conditions.
+    For example, what happens when par1 == par2 == par3? In addition, what happens if we change the order of the parameters
+    """
+    assert is_angle_between(0, 1, 2) == 1
+    assert is_angle_between(0, 20, 360) == 0
+    assert is_angle_between(-20, 10, 40) == 1
+    assert is_angle_between(0, 30, 60) == 1
+    assert is_angle_between(0, 0, 0) == 1
+    assert is_angle_between(10, 0, 5) == 0
+    assert is_angle_between(180, 135, 90) == 1
+    assert is_angle_between(360, 720, 90) == 0
+    assert is_angle_between(1220, 120, -240) == 1
+
+    # Checks negative angle conditions such as can it identify a negative angle between a positive and negative angle.
+    assert is_angle_between(-170, -130, -90) == 1
+    assert is_angle_between(-170, -130, 100) == 0
+    assert is_angle_between(-135, 20, -90) == 0
+    assert is_angle_between(-90, -90, -90) == 1
+    assert is_angle_between(180, 100, -233) == 1
